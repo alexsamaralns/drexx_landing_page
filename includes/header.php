e103d378b97a1547842f05da5d6d20270b730446
alexsamaralns <alexsamaralns@gmail.com>
2020-05-28 12:17:55 -0300

    drexx-landing-page

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<header>
+    <div id="showcase" class="navbar-items">
+    <nav class='cf'>
+        <ul class='cf'>
+            <li>
+                <a href='#' id='openup' class="logo"><img src="img/logo.png" alt=""></a>
+            </li>
+            <li class="options">
+                <a href='https://facebook.com' target="_blank"><img class="icons-navbar" src="img/facebook.png" alt=""></a>
+            </li>
+            <li class="options">
+                <a href='https://instagram.com' target="_blank"><img class="icons-navbar" src="img/instagram.png" alt=""></a>
+            </li>
+            <li class="options">
+                <a href='https://whatsapp.com' target="_blank"><img class="icons-navbar" src="img/whatsapp.png" alt=""></a>
+            </li>
+            <li class="options">
+                <a href='https://whatsapp.com' target="_blank"><img class="icons-navbar" src="img/email.png" alt=""></a>
+            </li>
+            <li class="options">
+                <a href='https://drexx.com.br' target="_blank"><img class="icons-navbar" src="img/shop.png" alt=""></a>
+            </li>
+        </ul>
+        </nav>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    
+    <?php require_once 'includes/head.php'; ?>
+
+</head>
+<body>
+    
+    <?php require_once 'includes/header.php'; ?>
+
+    <section>
+
+            <div id="slider">
+                <div class="photos">
+                    <div class="inline">
+                        <div>
+                            <img class="img-carosel" src="img/products/234.jpg">
+                        </div>
+                        <div>
+                            <img class="img-carosel" src="img/products/REF_63421_ ROSE.jpg">
+                        </div>
+                        <div>
+                            <img class="img-carosel" src="img/products/BB_12 JEANSPRETOXADREZ.jpg">
+                        </div>
+                    </div>
+                </div>
+            </div>    
+
+        <div class="section-main container">
+            <h1>Main Title</h1>
+        </div>
+    </section>
+
+    <?php require_once 'includes/footer.php'; ?>
+
+    <?php require_once 'includes/script.php'; ?>
+</body>
+</html>
