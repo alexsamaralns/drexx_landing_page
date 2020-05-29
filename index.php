a1fe552908df7474d3df86821247bad1807bbfe7
alexsamaralns <alexsamaralns@gmail.com>
2020-05-29 13:18:18 -0300

    desenvolvendo auto-slide

--- index.php
+++ index.php
@@ -9,27 +9,47 @@
     
     <?php require_once 'includes/header.php'; ?>
 
-    <section>
+    <section class="img-carosel">
 
-            <div id="slider">
-                <div class="photos">
-                    <div class="inline">
-                        <div>
-                            <img class="img-carosel" src="img/products/234.jpg">
-                        </div>
-                        <div>
-                            <img class="img-carosel" src="img/products/REF_63421_ ROSE.jpg">
-                        </div>
-                        <div>
-                            <img class="img-carosel" src="img/products/BB_12 JEANSPRETOXADREZ.jpg">
-                        </div>
-                    </div>
-                </div>
-            </div>    
+        <!-- 	INPUT FOR BUTTON SELECT SLIDE -->
+        <!-- <input type="radio" name="next" id="slide1"> -->
+        <!-- <input type="radio" name="next" id="slide2"> -->
+        <!-- <input type="radio" name="next" id="slide3"> -->
+        <!-- <input type="radio" name="next" id="slide4"> -->
 
-        <div class="section-main container">
-            <h1>Main Title</h1>
-        </div>
+        <!-- 	INPUT FOR BUTTON STOP&PLAY -->
+        <input type="radio" name="sto" id="play" checked>
+        <input type="radio" name="sto" id="stop">
+
+        <!-- CONTAINER -->
+        <div class="cont">
+
+            <!-- BUTTON PLAY&STOP -->
+            <!-- <label for="play" class="butPlay" onclick=""></label> -->
+            <!-- <label for="stop" class="butStop" onclick=""></label> -->
+
+            <!-- IMAGE -->
+            <div class="image">
+                <img src="http://s017.radikal.ru/i432/1310/37/f331c96eef02.jpg" class="oneP" alt="Flash">
+                <img src="http://s004.radikal.ru/i208/1310/09/7a8e37a9e719.jpg" class="twoP" alt="Superman">
+                <img src="http://s020.radikal.ru/i713/1310/5f/a15fcdeb8fe8.jpg" class="threeP" alt="Team">
+                <img src="http://s019.radikal.ru/i639/1310/02/1e3d78802280.jpg" class="fourP" alt="Joker">
+            </div>
+
+            <!-- ANIMATION LINE -->
+            <!-- <div class="line"></div> --> 
+
+            <!-- BOTTOM LINE -->
+            <!-- <div class="but"> -->
+                <!-- <label for="slide1" class="select1" onclick=""></label> -->
+                <!-- <label for="slide2" class="select2" onclick=""></label> -->
+                <!-- <label for="slide3" class="select3" onclick=""></label> -->
+                <!-- <label for="slide4" class="select4" onclick=""></label> -->
+                <!-- <div class="tooltip"></div> -->
+            <!-- </div> -->
+
+        </div>     
+                       
     </section>
 
     <?php require_once 'includes/footer.php'; ?>
