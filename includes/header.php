006fd625f8862d2fcf962532ad9ccc9d7475f061
alexsamaralns <alexsamaralns@gmail.com>
2020-05-30 19:41:48 -0300

    sections drexx-landing-page

--- includes/header.php
+++ includes/header.php
@@ -1,26 +1,31 @@
-<header>
-    <div id="showcase" class="navbar-items">
-        <nav class='cf'>
-            <ul class='cf'>
-                <li>
-                    <a href='#' id='openup'><img class="logo" src="img/drexx2.png" alt=""></a>
-                </li>
-                <li class="options">
-                    <a href='https://facebook.com' target="_blank"><img class="icons-navbar" src="img/facebook.png" alt=""></a>
-                </li>
-                <li class="options">
-                    <a href='https://instagram.com' target="_blank"><img class="icons-navbar" src="img/instagram.png" alt=""></a>
-                </li>
-                <li class="options">
-                    <a href='https://whatsapp.com' target="_blank"><img class="icons-navbar" src="img/whatsapp.png" alt=""></a>
-                </li>
-                <li class="options">
-                    <a href='https://whatsapp.com' target="_blank"><img class="icons-navbar" src="img/email.png" alt=""></a>
-                </li>
-                <li class="options">
-                    <a href='https://drexx.com.br' target="_blank"><img class="icons-navbar" src="img/shop.png" alt=""></a>
-                </li>
-            </ul>
-        </nav>
+<header class="cabecalho">
+    <div class="logo">
+        <div class="logo-text">
+            <a href='#' id='openup'><img class="logo" src="img/drexx2.png" alt=""></a>
+        </div>
+        <div class="logo-text">
+            <p class="logo-text">
+                Calçados e acessórios femininos, masculinos e infantis com os melhores preços.
+            </p>
+        </div>
+    </div>
+    <div class="nav-items">
+        <ul class='cf'>
+            <li class="options">
+                <a href='https://www.facebook.com/drexx.online' target="_blank"><img class="icons-navbar" src="img/facebook.png" alt=""></a>
+            </li>
+            <li class="options">
+                <a href='https://www.instagram.com/drexx.online/?hl=pt-br' target="_blank"><img class="icons-navbar" src="img/instagram.png" alt=""></a>
+            </li>
+            <li class="options">
+                <a href='https://wa.me/5537991820031' target="_blank"><img class="icons-navbar" src="img/whatsapp.png" alt=""></a>
+            </li>
+            <li class="options">
+                <a href='mailto:vendas1@drexx.com.br' target="_blank"><img class="icons-navbar" src="img/email.png" alt=""></a>
+            </li>
+            <li class="options">
+                <a href='https://eshops.mercadolivre.com.br/drexx+online' target="_blank"><img class="icons-navbar" src="img/shop.png" alt=""></a>
+            </li>
+        </ul>
     </div>
 </header>
--- index.php
+++ index.php
@@ -10,46 +10,71 @@
     <?php require_once 'includes/header.php'; ?>
 
     <section class="img-carosel">
+        <div class="w3-content w3-section">
+                <a href="https://produto.mercadolivre.com.br/MLB-1486748676-tenis-feminino-sapatenis-street-casual-sola-alta-macio-leve-_JM?searchVariation=53337823644&quantity=1#searchVariation=53337823644&position=34&type=item&tracking_id=2cdfd69b-f81b-4a57-8fa9-195b9d4d4c95" target="_blank">
+                    <img class="mySlides w3-animate-left" src="https://lh3.googleusercontent.com/qGNo8082CJ5Z_vTLjn11-f_Ddtyk3-HMPqB2AN27h6PRdu4bfPKtiDlFBVYA6qWM-OqOY6vMFzcDmh2_TVmaiK4TVGyRpRTEcVcpPjJ-8No9FkyQQPfhkI-VsF-_rpoK5cFdGPWJImcCRnknTZ5AVg9deXgm8koFrJ0PLFhP-UoqUkOu3VoZxyBSax_Iijvm5T6lfLtUxOWuDGMA5Hia3CMLvsL3pxuygNyjaqyAqDKHBFTs_N7tLhpYhvyBqK2JIse5PMwGD-QRw_Jlo_WYzpHbqJEWKowmgXVE38JAUAXAGmEY36oA7Umxq5SpXoO3UlAraMG7Lp2OepleWzqte9MjBvtOo-bA2qSRBseHcScbURACtTWC9YPvvkEvjk_KCrTc5UQTiKxU0Rd0Cv1jbWbip6pWEVqtzIytEEg0HWz9Dofaf8RUVr5hpaXxRYANCCnIV2KMfBfo2UvhJsZGa6pbsrCMT0B4c4OtI2C_hz0h5TrLl7apxBt6N1ksJVAxVqAv5DlW2b8DZNNfVaFwnk_YcneND2DtN2jWx9NMNhlfPl4uHQjX2IB-jr5EfD3FumXUPNjMFicoI0popYDLf2B3-qiCRJHfB8D1qowuZkdlkduUt_yqOt-MDUGKjwtcuNGd05-DffSBXU0_UGSJ_J8B562WeLt3yPfZdGGgV2otxB0GN4UWz2LSa6bU=s796-no?authuser=0" alt="img1">
+                </a>
+                <a href="https://produto.mercadolivre.com.br/MLB-1486705057-tnis-masculino-infantil-baby-bebe-com-fechamento-em-velcro-_JM?searchVariation=53336064706&quantity=1#searchVariation=53336064706&position=8&type=item&tracking_id=0e09f41a-cb2e-4b0d-b3fd-fd2f77e1d1b3" target="_blank">
+                    <img class="mySlides w3-animate-left" src="https://lh3.googleusercontent.com/pw/ACtC-3dLaO24ybwGAGcSn4DXMMz21jyQCwcrRC7Un8M9bXImQNVXEtjpq4z3kpNFAQfhjK0mbPJIJWpVtY2MpfyHu7lhapoCGhsBBfEq-3P8TpA6WHaDzO9xPzjrRkZss5-4LtmNElU5g2EzSVn21pnJebS3=s748-no?authuser=0" alt="img2">
+                </a>
+                <a href="https://produto.mercadolivre.com.br/MLB-1492141903-tnis-infantil-feminino-personagem-colorido-lona-003-_JM?searchVariation=53642583241&quantity=1#position=7&type=item&tracking_id=89a057f5-b74c-4f14-aae3-4d109c8ab36f" target="_blank">
+                    <img class="mySlides w3-animate-left" src="https://lh3.googleusercontent.com/pw/ACtC-3cjqN1kJVKN0C8dxZeoddHFXVXgKhrnu6gMQdmQN3WivC-6riwmaTVSIv9eNQCo6uAD-9Ghqwkur467ClHCPJKfzXF2BMR2qG4XtFOuTfnCzXmAcadycQc7xG6y8UH3DH0r2UMdurqAOlaZPUvROlEM=s796-no?authuser=0" alt="img3">
+                </a>
+                <div class="w3-center w3-display-bottommiddle" style="width:100%">
+                    <div class="w3-left" onclick="plusDivs(1)"><img class="left-arrow" src="img/expand-arrow-left.png"></div>
+                    <div class="w3-right" onclick="plusDivs(-1)"><img class="right-arrow" src="img/expand-arrow-right.png"></div>
+                </div>               
+        </div>
+    </section>
 
-        <!-- 	INPUT FOR BUTTON SELECT SLIDE -->
-        <!-- <input type="radio" name="next" id="slide1"> -->
-        <!-- <input type="radio" name="next" id="slide2"> -->
-        <!-- <input type="radio" name="next" id="slide3"> -->
-        <!-- <input type="radio" name="next" id="slide4"> -->
+    <section class="intro">
+        <h1>Drexx Online</h1>
+        <p>
+            Temos como objetivo apresentar produtos para você, cliente, que deseja aliar conforto e moda.<br>
+            Nossa missão é oferecer produtos de qualidade com um preço justo!<br>
+            E para isso, mantemos fortemente como base os nossos valores que são:<br>
+            integridade, respeito com as pessoas, simplicidade e agilidade.<br>
+        </p>
+        <img src="img/drexxCoroaBranca.png">
+        <p>Sempre com você <strong style="font-size: 32px;">♥</strong></p>
+    </section>
 
-        <!-- 	INPUT FOR BUTTON STOP&PLAY -->
-        <input type="radio" name="sto" id="play" checked>
-        <input type="radio" name="sto" id="stop">
+    <section class="e-commerce">
+        <img src="https://lh3.googleusercontent.com/pw/ACtC-3fet9Uk83FePUFYNooiT8Y9eDTKbk5jnDNo4x83rr6w2qcOYs5Otli5skDK1yEyy89yUrT9Ra38ccHoB_jRqPq6uf0sRMmMq9X_23HZLVLDNzeGi3BMjshjyEosG2x3DbiLeu29fa8NNSuJsiHG8PIl=s748-no?authuser=0">
+    </section>
 
-        <!-- CONTAINER -->
-        <div class="cont">
-
-            <!-- BUTTON PLAY&STOP -->
-            <!-- <label for="play" class="butPlay" onclick=""></label> -->
-            <!-- <label for="stop" class="butStop" onclick=""></label> -->
-
-            <!-- IMAGE -->
-            <div class="image">
-                <img src="http://s017.radikal.ru/i432/1310/37/f331c96eef02.jpg" class="oneP" alt="Flash">
-                <img src="http://s004.radikal.ru/i208/1310/09/7a8e37a9e719.jpg" class="twoP" alt="Superman">
-                <img src="http://s020.radikal.ru/i713/1310/5f/a15fcdeb8fe8.jpg" class="threeP" alt="Team">
-                <img src="http://s019.radikal.ru/i639/1310/02/1e3d78802280.jpg" class="fourP" alt="Joker">
+    <section class="buy-options">
+        <div class="buy-style">
+            <div class="varejo">
+                <h2>VAREJO</h2>
+                <p>
+                    Leverage agile frameworks to provide a robust synopsis for high level overviews.
+                    Iterative approaches to corporate strategy foster collaborative thinking to further
+                    the overall value proposition. Organically grow the holistic world view of disruptive
+                    innovation via workplace diversity and empowerment.
+                </p>
+                <img class="varejo-icon" src="img/varejo.png">
             </div>
-
-            <!-- ANIMATION LINE -->
-            <!-- <div class="line"></div> --> 
-
-            <!-- BOTTOM LINE -->
-            <!-- <div class="but"> -->
-                <!-- <label for="slide1" class="select1" onclick=""></label> -->
-                <!-- <label for="slide2" class="select2" onclick=""></label> -->
-                <!-- <label for="slide3" class="select3" onclick=""></label> -->
-                <!-- <label for="slide4" class="select4" onclick=""></label> -->
-                <!-- <div class="tooltip"></div> -->
-            <!-- </div> -->
-
-        </div>     
-                       
+            <div class="atacado">
+                <h2>ATACADO</h2>
+                <p>
+                    Bring to the table win-win survival strategies to ensure proactive domination.
+                    At the end of the day, going forward, a new normal that has evolved from generation X
+                    is on the runway heading towards a streamlined cloud solution. User generated content
+                    in real-time will have multiple touchpoints for offshoring
+                </p>
+                <img class="atacado-icon" src="img/atacado.png">
+            </div>
+            <div class="atacarejo">
+                <h2>ATACAREJO</h2>
+                <p>
+                    Capitalize on low hanging fruit to identify a ballpark value added activity to beta test.
+                    Override the digital divide with additional clickthroughs from DevOps. Nanotechnology
+                    immersion along the information highway will close the loop on focusing solely on the bottom line.
+                </p>
+                <img class="atacarejo-icon" src="img/atacarejo.png">
+            </div>
+        </div>
     </section>
 
     <?php require_once 'includes/footer.php'; ?>
